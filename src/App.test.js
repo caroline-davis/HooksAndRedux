1183e8bf271643e42ca542cf8a895fe2d18a880d
Caroline Davis <caroline.davis@mantelgroup.com.au>
2020-01-10 15:16:05 +1100

    fix: Snapshot test for app.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,15 @@ import { render } from '@testing-library/react';
 import App from './App';
 import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
+import { createStore } from "redux";
+import { Provider } from 'react-redux';
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
 
 describe('App component', () => {
   it('matches the snapshot', () => {
-    const tree  = renderer.create(<App />).toJSON();
+    const tree  = renderer.create(<Provider store={store}><App /></Provider>).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
--- src/components/Save.test.js
+++ src/components/Save.test.js
@@ -3,9 +3,14 @@ import Save from './Save';
 import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 
-describe('Save Name component', () => {
+describe('Save button component', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<Save />);
+    })
+
     it('adds the text to screen when clicked', () => {
-        const wrapper = shallow(<Save />);
         expect(wrapper.exists()).toBe(true);   
     });
 });
